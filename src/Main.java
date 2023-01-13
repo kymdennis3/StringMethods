4fa44ebf79ecd272a2bf3ca2a024d3ba68548ba1
Kimtai <kymdennis3@gmail.com>
2023-01-13 05:47:23 +0300

    equals() is case sensitive

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //String - a reference data type that can store one or more characters
+        //           reference data types have access to useful methods
+
+        String name = "Bro";
+
+        boolean result = name.equals("bro");
+        System.out.println(result);
+
     }
 }
