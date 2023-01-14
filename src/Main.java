36809b28b70d6565035a6f478a96b311b9a3b9d1
Kimtai <kymdennis3@gmail.com>
2023-01-14 12:48:07 +0300

    replace(old char, new char) - replaces an old character with a new character

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         String name = "    Bro      ";
 
         //boolean result = name.equalsIgnoreCase("bro");
-        //int result = name.length();
-        char result = name.charAt(1);
+        int result = name.length();
+        //char result = name.charAt(1);
         //int result = name.indexOf("o");
         //boolean result = name.isEmpty();
         //String result = name.toUpperCase();
